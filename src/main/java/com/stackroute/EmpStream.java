b65ba11366582fea678d78a4872a3dd70d01e4a7
shivanisajjan <shivanisajjan269@gmail.com>
2019-09-19 22:53:15 +0530

    First commit

--- src/main/java/com/stackroute/EmpStream.java
+++ src/main/java/com/stackroute/EmpStream.java
@@ -61,7 +61,7 @@ public class EmpStream
 
         System.out.println("All employee having mobile numbers 8552802816:");
         System.out.println("-----------------------------------------------");
-        List<Employee> n2 = li.stream().filter(str->getemployeeph(str)==true).collect(Collectors.toList());
+        List<Employee> n2=li.stream().filter(str->str.getMobileNumber().stream().filter(str1->str1.getNumber()==8552802816L).findFirst().isPresent()).collect(Collectors.toList());
         for (Employee emp:n2){
             System.out.println(emp.toString());
         }
@@ -73,48 +73,98 @@ public class EmpStream
          *****************************************************/
         System.out.println("All employee having mobile numbers 8552802816 and 9145534362:");
         System.out.println("---------------------------------------------------------------");
-        List<Employee> n3 = li.stream().filter(str->getemployeeph(str)==true).collect(Collectors.toList());
+        List<Employee> n3 = li.stream().filter(str->str.getMobileNumber().stream().filter(str1->str1.getNumber()==8552802816L)
+                .findFirst().isPresent() && str.getMobileNumber().stream().filter(str1->str1.getNumber()==9145534362L)
+                .findFirst().isPresent()).collect(Collectors.toList());
         for (Employee emp:n3){
             System.out.println(emp.toString());
         }
-//        System.out.println("____________________________________________________________________________" +
-//                "_____________________________________________________________________");
+        System.out.println("____________________________________________________________________________" +
+                "_____________________________________________________________________");
 
 
 
+        List<TempEmployee> al=new ArrayList<>();
+        TempEmployee te1=new TempEmployee("Shivani",21,new Address(509103),Arrays.asList(new MobileNumber(8552802816L),
+                new MobileNumber(9145534362L)));
+        TempEmployee te2=new TempEmployee("Jayesh",20,new Address(509104),Arrays.asList(new MobileNumber(8552802816L),
+                new MobileNumber(9145533332L)));
+        TempEmployee te3=new TempEmployee("Hari Priya",22,new Address(509103),Arrays.asList(new MobileNumber(8552123416L),
+                new MobileNumber(9145312332L)));
+        TempEmployee te4=new TempEmployee("Jayesh",19,new Address(509106),Arrays.asList(new MobileNumber(8552802818L),
+                new MobileNumber(9145534362L)));
+        TempEmployee te5=new TempEmployee("Deepak",23,new Address(509107),Arrays.asList(new MobileNumber(8552802816L),
+                new MobileNumber(9145534361L)));
+        al.add(te1);
+        al.add(te2);
+        al.add(te3);
+        al.add(te4);
+        al.add(te5);
 
         /*****************************************************
          Create a List<Employee> from the List<TempEmployee> using stream
          *****************************************************/
-
-
+        System.out.println("Creating a List<Employee> from the List<TempEmployee>:");
+        System.out.println("---------------------------------------------------------------");
+        List<Employee> employees = al.stream()
+                .map(p -> new Employee(p.getName(), p.getAge(),p.getAddress(),p.getMobileNumber()))
+                .collect(Collectors.toList());
+        for (Employee emp:employees){
+            System.out.println(emp.toString());
+        }
+        System.out.println("____________________________________________________________________________" +
+                "_____________________________________________________________________");
 
 
         /*****************************************************
          Convert List<Employee> to List<String> of employee name using stream
          *****************************************************/
-
-
-
+        System.out.println("Convert List<Employee> to List<String> of employee name:");
+        System.out.println("---------------------------------------------------------------");
+        List<String> empname=li.stream().map(p->p.getName()).collect(Collectors.toList());
+        for (String emp:empname){
+            System.out.println(emp.toString());
+        }
+        System.out.println("____________________________________________________________________________" +
+                "_____________________________________________________________________");
 
         /*****************************************************
          Convert List<employees> to String using stream
          *****************************************************/
+        System.out.println("Converting List<employees> to String:");
+        System.out.println("-----------------------------------------");
+        String employeeList = li.stream()
+                .map(x -> x.toString())
+                .collect(Collectors.joining(", "));
+        System.out.println(employeeList.toString());
 
 
 
         /*****************************************************
          Change the case of List<String> using stream
          *****************************************************/
-
-
+        System.out.println("Change the case of List<String>:");
+        System.out.println("-------------------------------------");
+        List<String> toUpper=empname.stream().map(name->name.toUpperCase()).collect(Collectors.toList());
+        for (String emp:toUpper){
+            System.out.println(emp.toString());
+        }
+        System.out.println("____________________________________________________________________________" +
+                "_____________________________________________________________________");
 
 
         /*****************************************************
          Sort List<String> using stream
          *****************************************************/
-
-
+        System.out.println("Sorting List<String>:");
+        System.out.println("-------------------------------------");
+        List<String> empnamesort = empname.stream().sorted()
+                .collect(Collectors.toList());
+        for (String emp:empnamesort){
+            System.out.println(emp.toString());
+        }
+        System.out.println("____________________________________________________________________________" +
+                "_____________________________________________________________________");
 
 
         /*****************************************************
@@ -125,20 +175,28 @@ public class EmpStream
 
 
     }
+//    private static boolean getemployeephone(Employee str) {
+//        List<MobileNumber> al=str.getMobileNumber();
+//        for(int i=0;i<al.size();i++){
+//            if(al.get(i).getNumber()==8552802816L){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
-    private static boolean getemployeeph(Employee str) {
-//        Employee dummy=new Employee("false",0,new Address(0),Arrays.asList(new MobileNumber(8552802816L)));
-        List<MobileNumber> al=str.getMobileNumber();
-        for(int i=0;i<al.size();i++){
-            if(al.get(i).getNumber()==8552802816L){
-                for(int j=i+1;j<al.size();j++){
-                    if(al.get(j).getNumber()==9145534362L){
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
+//    private static boolean getemployeephone1(Employee str) {
+//        List<MobileNumber> al=str.getMobileNumber();
+//        for(int i=0;i<al.size();i++){
+//            if(al.get(i).getNumber()==8552802816L){
+//                for(int j=i+1;j<al.size();j++){
+//                    if(al.get(j).getNumber()==9145534362L){
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//    }
 
 }
--- src/main/java/com/stackroute/TempEmployee.java
+++ src/main/java/com/stackroute/TempEmployee.java
@@ -1,10 +1,64 @@
 package com.stackroute;
 
 
+import java.util.List;
 
 /**
  TempEmployee Class consist of parameters: name, age, address of type Address, mobileNumber of type List<MobileNumber>
  */
 public class TempEmployee {
+    private String name;
+    private Integer age;
+    private Address address;
+    private List<MobileNumber> mobileNumber;
+
+    TempEmployee(String name,Integer age,Address address,List<MobileNumber> mobileNumber){
+        this.name=name;
+        this.age=age;
+        this.address=address;
+        this.mobileNumber=mobileNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                ", mobileNumber=" + mobileNumber +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<MobileNumber> getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(List<MobileNumber> mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 
 }
